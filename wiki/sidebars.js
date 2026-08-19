85b89e2bf929479cb83982aeb39e7288c65c6048
MyNDIS <myndiswiki@gmail.com>
2026-08-19 21:39:22 +1000

    Add wiki navigation

---
+++ wiki/sidebars.js
@@ -0,0 +1,25 @@
+module.exports = {
+  wikiSidebar: [
+    'home',
+    {
+      type: 'category',
+      label: 'I want to apply for the NDIS',
+      items: ['apply/index'],
+    },
+    {
+      type: 'category',
+      label: 'I am on the NDIS',
+      items: ['on-the-ndis/index'],
+    },
+    {
+      type: 'category',
+      label: 'Questions',
+      items: ['questions/index'],
+    },
+    {
+      type: 'category',
+      label: 'Keywords',
+      items: ['keywords/index'],
+    },
+  ],
+};
